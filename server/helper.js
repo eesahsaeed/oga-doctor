eff76ef0ca5cffa51217cb4d6de974b7727afdde
eesah saeed <eesahsaeed@gmail.com>
2026-04-29 13:17:21 +0100

    fixed video consultation 1

--- server/helper.js
+++ server/helper.js
@@ -1,7 +1,6 @@
-
 export const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID || '';
 export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY || '';
-export const AWS_REGION = process.env.AWS_REGION || "us-east-1";
+export const AWS_REGION = process.env.AWS_REGION || 'us-east-1';
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'ogadoctor-secret';
 
@@ -9,6 +8,7 @@ export const JWT_SECRET = process.env.JWT_SECRET || 'ogadoctor-secret';
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || '';
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || '';
 export const GOOGLE_WEB_CLIENT_ID = process.env.GOOGLE_WEB_CLIENT_ID || '';
-export const GOOGLE_ANDROID_CLIENT_ID = process.env.GOOGLE_ANDROID_CLIENT_ID || '';
+export const GOOGLE_ANDROID_CLIENT_ID =
+  process.env.GOOGLE_ANDROID_CLIENT_ID || '';
 export const GOOGLE_IOS_CLIENT_ID = process.env.GOOGLE_IOS_CLIENT_ID || '';
 export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI || '';
